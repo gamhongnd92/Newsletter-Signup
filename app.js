bc716e76b196512502a196aa1b116a3aa5922b05
Gam <trant48@student.douglascollege.ca>
2020-12-28 22:24:21 -0800

    add post and get

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.use(express.static("public"));
+
+// app.post("/", function(req, res) {
+//     var firstName = req.body.inputFirstName;
+//     var lastName = req.body.inputLastName;
+//     var email = req.body.inputEmail;
+// })
+
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
